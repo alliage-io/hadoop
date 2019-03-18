52c3a232741b68112cbb7f9831ff1c714db424eb
Eric Yang <eyang@apache.org>
2019-03-18 13:04:49 -0400

    HADOOP-16167. Fixed Hadoop shell script for Ubuntu 18. Contributed by Daniel Templeton

--- hadoop-common-project/hadoop-common/src/main/bin/hadoop-functions.sh
+++ hadoop-common-project/hadoop-common/src/main/bin/hadoop-functions.sh
@@ -2355,6 +2355,10 @@ function hadoop_verify_user_perm
   declare command=$2
   declare uvar
 
+  if [[ ${command} =~ \. ]]; then
+    return 1
+  fi
+
   uvar=$(hadoop_build_custom_subcmd_var "${program}" "${command}" USER)
 
   if [[ -n ${!uvar} ]]; then
@@ -2383,6 +2387,10 @@ function hadoop_need_reexec
   # we've already been re-execed, bail
 
   if [[ "${HADOOP_REEXECED_CMD}" = true ]]; then
+    return 1
+  fi
+
+  if [[ ${command} =~ \. ]]; then
     return 1
   fi
 
@@ -2419,6 +2427,10 @@ function hadoop_subcommand_opts
   declare ucommand
 
   if [[ -z "${program}" || -z "${command}" ]]; then
+    return 1
+  fi
+
+  if [[ ${command} =~ \. ]]; then
     return 1
   fi
 
